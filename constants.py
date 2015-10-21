5fd2444f9c0aba7bdad87434af25ca0df2659130
jswift <jswift@thacher.org>
2015-10-21 14:04:54 -0700

    added solar luminosity, stefan-boltzmann constant

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ G      = 6.67259e-8
 # Astronomical Constants
 Msun   = 1.98892e33
 Rsun   = 6.955e10
+Lsun   = 3.839e33
 Rearth = 6.3781e8
 Mearth = 5.97219e27
 AU     = 1.49597871e13
@@ -14,3 +15,4 @@ k  = 1.380658e-16
 mp = 1.6726231e-24	
 mH = 1.6733e-24
 c  = 2.99792458e10
+sb = 5.6704e-5
