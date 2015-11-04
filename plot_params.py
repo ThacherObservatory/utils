bf7deb4c449ac0095fb4a846ed68dae29ef6ebe5
jswift <jswift@thacher.org>
2015-11-04 13:59:53 -0800

    add plot_params to repository

---
+++ plot_params.py
@@ -0,0 +1,20 @@
+import matplotlib as mpl
+
+def plot_params(fontsize=16,linewidth=1.5):
+    """
+    Procedure to set the parameters for this suite of plotting utilities
+    """
+    
+    mpl.rcParams['axes.linewidth'] = linewidth
+    mpl.rcParams['xtick.major.size'] = 5
+    mpl.rcParams['xtick.major.width'] = linewidth
+    mpl.rcParams['ytick.major.size'] = 5
+    mpl.rcParams['ytick.major.width'] = linewidth
+    mpl.rcParams['xtick.labelsize'] = fontsize
+    mpl.rcParams['ytick.labelsize'] = fontsize
+
+    return
+
+def plot_defaults():
+    mpl.rcdefaults()
+    return
